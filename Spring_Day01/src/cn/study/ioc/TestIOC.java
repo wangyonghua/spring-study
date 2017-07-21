2c917fe2af75410c95e8c850c942e7fba22d6e31
wangyonghua <wyh@2016>
2017-07-21 19:03:19 +0800

    静态工厂得到实例

--- Spring_Day01/src/cn/study/ioc/TestIOC.java
+++ Spring_Day01/src/cn/study/ioc/TestIOC.java
@@ -4,11 +4,21 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.study.bean.Bean2;
+
 public class TestIOC {
 	@Test
 	public void testUser() {
 		ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
 		User user = (User) context.getBean("user");
+		System.out.println(user);
+		user.add();
+	}
+
+	@Test
+	public void testUser1() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+		Bean2 user = (Bean2) context.getBean("bean2");
 		System.out.println(user);
 		user.add();
 	}
---
+++ Spring_Day01/src/com/study/bean/Bean2Factory.java
@@ -0,0 +1,7 @@
+package com.study.bean;
+
+public class Bean2Factory {
+	public static Bean2 getBean2() {
+		return new Bean2();
+	}
+}
