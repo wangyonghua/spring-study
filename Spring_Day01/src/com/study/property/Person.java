62ae6c34f70dad46f40c85a86ba67e2722041839
wangyonghua <1770220534@qq.com>
2017-07-22 23:23:25 +0800

    复杂属性注入

--- Spring_Day01/src/com/study/property/Person.java
+++ Spring_Day01/src/com/study/property/Person.java
@@ -1,5 +1,9 @@
 package com.study.property;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
 public class Person {
 	private String pname;
 
@@ -7,7 +11,31 @@ public class Person {
 		this.pname = pname;
 	}
 
+	private String[] arr;
+	private List<String> list;
+	private Map<String, String> map;
+	private Properties properties;
+
+	public void setArr(String[] arr) {
+		this.arr = arr;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
 	public void test1() {
-		System.out.println("test1212122" + this.pname);
+		System.out.println("arr" + this.arr);
+		System.out.println("list" + this.list);
+		System.out.println("map" + this.map);
+		System.out.println("properties" + this.properties);
 	}
 }
