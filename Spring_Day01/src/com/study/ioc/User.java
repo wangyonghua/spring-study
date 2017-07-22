863f400f3eba2e67e35bd6ea91671c77fbecf8ad
wangyonghua <1770220534@qq.com>
2017-07-22 19:48:57 +0800

    有参构造注入属性

---
+++ Spring_Day01/src/com/study/ioc/User.java
@@ -0,0 +1,25 @@
+package com.study.ioc;
+
+public class User {
+	private String username;
+
+	public User(String username) {
+		this.setUsername(username);
+	}
+
+	public User() {
+
+	}
+
+	public void add() {
+		System.out.println("add....");
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
---
+++ Spring_Day01/src/com/study/property/PropertyDemo1.java
@@ -0,0 +1,21 @@
+package com.study.property;
+
+public class PropertyDemo1 {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public PropertyDemo1(String username) {
+		this.username = username;
+	}
+
+	public void test1Demo() {
+		System.out.println("test1 demo1....");
+	}
+}
