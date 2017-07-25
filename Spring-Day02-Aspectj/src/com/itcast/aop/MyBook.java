e5ec80fc404ff05a5d9820087a9f897cfc7d3188
wangyonghua <wyh@2016>
2017-07-25 10:50:20 +0800

    aspectj多种类型的增强

--- Spring-Day02-Aspectj/src/com/itcast/aop/MyBook.java
+++ Spring-Day02-Aspectj/src/com/itcast/aop/MyBook.java
@@ -1,7 +1,19 @@
 package com.itcast.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class MyBook {
 	public void before1() {
 		System.out.println("前置增强 ....");
+	}
+
+	public void after1() {
+		System.out.println("后置增强 ....");
+	}
+
+	public void around1(ProceedingJoinPoint joinPoint) throws Throwable {
+		System.out.println("方法执行之前");
+		joinPoint.proceed();
+		System.out.println("方法执行之后");
 	}
 }
