eed619b39cff2b63eb8a7f94b8af1e7747e8257b
wangyonghua <wyh@2016>
2017-07-25 14:26:09 +0800

    Log4j配置

--- Spring-Day02-Aspectj/src/com/itcast/test/TestBook.java
+++ Spring-Day02-Aspectj/src/com/itcast/test/TestBook.java
@@ -1,5 +1,6 @@
 package com.itcast.test;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,11 +8,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.itcast.aop.Book;
 
 public class TestBook {
-	private ApplicationContext context;
-
 	@Test
 	public void testBook() {
-		context = new ClassPathXmlApplicationContext("Bean3.xml");
+		Logger logger = Logger.getLogger(this.getClass());
+		logger.info("hello");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean3.xml");
 		Book book = (Book) context.getBean("book");
 		book.add();
 		System.out.println("okay");
