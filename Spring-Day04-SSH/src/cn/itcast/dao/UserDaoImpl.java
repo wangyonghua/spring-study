6d82c00957e68300f25fcae254a110caff15133d
wangyonghua <wyh@2016>
2017-08-15 14:13:43 +0800

    整合资料

--- Spring-Day04-SSH/src/cn/itcast/dao/UserDaoImpl.java
+++ Spring-Day04-SSH/src/cn/itcast/dao/UserDaoImpl.java
@@ -1,5 +1,7 @@
 package cn.itcast.dao;
 
+import java.util.List;
+
 import org.springframework.orm.hibernate4.HibernateTemplate;
 
 import cn.itcast.entity.User;
@@ -14,11 +16,30 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public void add() {
 		// TODO Auto-generated method stub
-		User user = new User();
-		user.setUid(1);
-		user.setUsername("wang");
-		user.setAddress("北京");
-		template.save(user);
+		// User user = new User();
+		// user.setUid(1);
+		// user.setUsername("wang");
+		// user.setAddress("北京");
+		// template.save(user);
+
+		// get 方法 :根据id查询
+		// User user2 = template.get(User.class, 1);
+		// System.out.println(user2.getUsername());
+
+		// find方法查询所有记录
+		List<User> list = (List<User>) template.find("from User");
+		for (User user : list) {
+			System.out.println(user);
+		}
+
+		// find 条件查询
+		List<User> list1 = (List<User>) template.find("from User where username=?", "lucy");
+		for (User user : list1) {
+			System.out.println(user);
+		}
+		
+		//find不能做分页
+
 	}
 
 }
